79b0c83904efa98fc154b220b962d3a724a16c51
babar578 <67875246+babar578@users.noreply.github.com>
2020-07-29 12:34:17 +0500

    uploadimage

--- MyShop/MyShop.UI/Controllers/ProductController.cs
+++ MyShop/MyShop.UI/Controllers/ProductController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -44,7 +45,7 @@ namespace MyShop.WebUI.Controllers
             return View(viewModel);
         }
         [HttpPost]
-        public ActionResult Create(Product product)
+        public ActionResult Create(Product product,HttpPostedFileBase file)
         {
             if (!ModelState.IsValid)
             {
@@ -52,6 +53,12 @@ namespace MyShop.WebUI.Controllers
             }
             else
             {
+                if (file!=null)
+                {
+                    product.Image = product.Id + Path.GetExtension(file.FileName);
+                    file.SaveAs(Server.MapPath("//Content//ProductImage//") + product.Image);
+                }
+             
                 context.Insert(product);
                 context.Commit();
                 return RedirectToAction("Index");
@@ -77,7 +84,7 @@ namespace MyShop.WebUI.Controllers
        }
 
         [HttpPost]
-        public ActionResult Edit(Product product, string id)
+        public ActionResult Edit(Product product, string id , HttpPostedFileBase file)
         {
             Product productToEdit = context.Find(id);
             
@@ -87,24 +94,23 @@ namespace MyShop.WebUI.Controllers
             }
             else
             {
-
                 if (!ModelState.IsValid)
                 {
                     return View(product);
                 }
+                if (file!=null)
+                {
+                    product.Image = product.Id + Path.GetExtension(file.FileName);
+                    file.SaveAs(Server.MapPath("//Content//ProductImage//") + product.Image);
+                }
                 productToEdit.Name = product.Name;
                 productToEdit.Description = product.Description;
-                productToEdit.Image = product.Image;
                 productToEdit.Price = product.Price;
                 productToEdit.Catagory = product.Catagory;
                 context.Commit();
                 return RedirectToAction("Index");
-
             }
-
-
         }
-
 
        public ActionResult Delete(string Id)
         {
